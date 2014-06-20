f0530405d481c7a46254a3d053de9631262f3651
Paul Kourany <pkourany@users.noreply.github.com>
2014-06-19 23:07:46 -0400

    Update MMA7660FC_Demo.ino

--- MMA7660FC_Demo.ino
+++ MMA7660FC_Demo.ino
@@ -1,11 +1,16 @@
 /*****************************************************************************/
-//	Function:    Get the accelemeter of the x/y/z axis. 
+//    Function:    Get the acc of the x/y/z axis.
 //  Hardware:    Grove - 3-Axis Digital Accelerometer(±1.5g)
-//	Arduino IDE: Arduino-1.0
-//	Author:	 Frankie.Chu		
-//	Date: 	 Jan 10,2013
-//	Version: v0.9b
-//	by www.seeedstudio.com
+//    Arduino IDE: Arduino-1.0
+//    Author:     Frankie.Chu
+//    Date:      Jan 10,2013
+//    Version: v0.9b
+//    by www.seeedstudio.com
+
+
+//  modify by loovee
+//  2013-9-25
+//  add time out
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -25,38 +30,52 @@
 
 //#include <Wire.h>
 #include "MMA7660.h"
+MMA7660 acc;
 
-MMA7660 accelemeter;
 void setup()
 {
-	Serial.begin(9600);
-	accelemeter.init();
+    acc.init();
+    pinMode(D7, OUTPUT);
+    Serial.begin(9600);
 }
+
 void loop()
 {
-	int8_t x;
-	int8_t y;
-	int8_t z;
-	float ax,ay,az;
-	accelemeter.getXYZ(&x,&y,&z);
-	
-	Serial.print("x = ");
-    Serial.println(x); 
+
+    static long cnt     = 0;
+    static long cntout  = 0;
+    float ax,ay,az;
+    int8_t x, y, z;
+
+    acc.getXYZ(&x,&y,&z);
+
+    Serial.print("x = ");
+    Serial.println(x);
     Serial.print("y = ");
-    Serial.println(y);   
+    Serial.println(y);
     Serial.print("z = ");
     Serial.println(z);
-	
-	accelemeter.getAcceleration(&ax,&ay,&az);
+
+
+    if(acc.getAcceleration(&ax,&ay,&az))
+    {
+        Serial.print("get data ok: ");
+    }
+    else
+    {
+        Serial.print("time out: ");
+    }
+    
     Serial.println("accleration of X/Y/Z: ");
-	Serial.print(ax);
-	Serial.println(" g");
-	Serial.print(ay);
-	Serial.println(" g");
-	Serial.print(az);
-	Serial.println(" g");
-	Serial.println("*************");
-	delay(500);
+    Serial.print(ax);
+    Serial.println(" g");
+    Serial.print(ay);
+    Serial.println(" g");
+    Serial.print(az);
+    Serial.println(" g");
+    Serial.println();
+    delay(50);
+
 }
 
 
